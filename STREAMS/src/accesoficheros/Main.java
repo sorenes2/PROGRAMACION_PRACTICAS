78613b9e94ff43daf3ba19b7c83023497578d647
Silvia <sorenes@ifp.es>
2023-02-21 19:41:32 +0100

    saludo

--- STREAMS/src/accesoficheros/Main.java
+++ STREAMS/src/accesoficheros/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		system.out.println("Hola soy pepito");
 		LeerFichero lf = new LeerFichero();
 		//lf.leer();
 		lf.leerConBuffer();
